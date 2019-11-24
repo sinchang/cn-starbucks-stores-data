ee517428fd1afc1d0b3ac74c308418c05c453dc5
sinchang <sinchangwen@gmail.com>
2019-11-24 21:38:11 +0800

    Add travis-push.sh

---
+++ travis-push.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+npm run build
+npm run check
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+echo ========== CHECKING FOR CHANGES ========
+changes=$(git diff format.json)
+if [ -n "$changes" ]; then
+  now=$(date +"%Y-%m-%d")
+  git checkout -b $now
+  git add .
+  git commit -m "Update $now"
+  git remote rm origin
+  git remote add origin https://sinchang:${GITHUB_TOKEN}@github.com/sinchang/cn-starbucks-stores-data.git > /dev/null 2>&1
+  git push origin master --quiet
+else
+  echo "... No changes found"
+fi
